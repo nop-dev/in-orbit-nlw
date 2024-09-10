2e77aab2c559e08e873e29e62463ea61ebba610f
Yuri Sousa <ysousa.dev@gmail.com>
2024-09-10 17:50:24 -0300

    feat: :sparkles: adiciona toda a base finalmente montada, pronto para a criação de funcionalidades

---
+++ server/src/db/schema.ts
@@ -0,0 +1,15 @@
+import { pgTable, text, integer, timestamp } from "drizzle-orm/pg-core";
+import { createId } from "@paralleldrive/cuid2";
+
+export const goals = pgTable('goals', {
+    id: text('id').primaryKey().$defaultFn(() => createId()),
+    title: text('title').notNull(),
+    desiredWeeklyFrequency: integer('desired_weekl_frequency').notNull(),
+    createdAt: timestamp('created_at', { withTimezone: true}).notNull().defaultNow(),
+})
+
+export const completedGoals = pgTable('completed_goals', {
+    id: text('id').primaryKey().$defaultFn(() => createId()),
+    goalId: text('goal_id').references(() => goals.id).notNull(),
+    
+})
