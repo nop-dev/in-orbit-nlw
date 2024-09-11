53a586e03dcc812ddc0cd7938f91a087ce926683
Yuri Sousa <ysousa.dev@gmail.com>
2024-09-10 22:08:01 -0300

    feat: :sparkles: adiciona e testa a primeira rota da aplicação, criação de metas (sujeito a alteração futura)

--- server/src/http/server.ts
+++ server/src/http/server.ts
@@ -1,11 +1,27 @@
 import fastify from "fastify";
+import { createGoal } from "../features/create-goals";
+import z, { number } from "zod";
 
 const app = fastify();
+
+app.post('/goals', async req => {
+	const createGoalSchema = z.object({
+		title: z.string(),
+		desiredWeeklyFrequency: z.number().int().min(1).max(7),
+	})
+
+	const body = createGoalSchema.parse(req.body);
+
+	await createGoal({
+		title: body.title,
+		desiredWeeklyFrequency: body.desiredWeeklyFrequency
+	})
+})
 
 app
 	.listen({
 		port: 3331,
 	})
 	.then(() => {
-		console.log("Server running!!!");
+		console.log("Server running!!! Port: 3331");
 	});
