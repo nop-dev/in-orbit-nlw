b82e5397abd460acddc3be07067d8a00d0331434
Yuri Sousa <ysousa.dev@gmail.com>
2024-09-11 15:58:43 -0300

    fix: :bug: resolve um pequeno problema do banco de dados

--- server/.migrations/0000_mean_landau.sql
+++ server/.migrations/0000_mean_landau.sql
@@ -1,6 +1,12 @@
 CREATE TABLE IF NOT EXISTS "completed_goals" (
 	"id" text PRIMARY KEY NOT NULL,
-	"goal_id" text NOT NULL,
+	"goal_id" text NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE IF NOT EXISTS "goals" (
+	"id" text PRIMARY KEY NOT NULL,
+	"title" text NOT NULL,
+	"desired_weekly_frequency" integer NOT NULL,
 	"created_at" timestamp with time zone DEFAULT now() NOT NULL
 );
 --> statement-breakpoint
--- server/src/db/schema.ts
+++ server/src/db/schema.ts
@@ -4,12 +4,14 @@ import { createId } from "@paralleldrive/cuid2";
 export const goals = pgTable('goals', {
     id: text('id').primaryKey().$defaultFn(() => createId()),
     title: text('title').notNull(),
-    desiredWeeklyFrequency: integer('desired_weekl_frequency').notNull(),
+    desiredWeeklyFrequency: integer('desired_weekly_frequency').notNull(),
     createdAt: timestamp('created_at', { withTimezone: true}).notNull().defaultNow(),
 })
 
 export const completedGoals = pgTable('completed_goals', {
     id: text('id').primaryKey().$defaultFn(() => createId()),
     goalId: text('goal_id').references(() => goals.id).notNull(),
-    
+    createdAt: timestamp('created_at', { withTimezone: true })
+        .notNull()
+        .defaultNow(),
 })
