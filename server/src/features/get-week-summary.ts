6e98b6f595cd416d5bd4866babc7961b0e53e4dc
Yuri Sousa <ysousa.dev@gmail.com>
2024-09-18 17:21:24 -0300

    feat: :sparkles: adiciona uma última função e rota para retornar o summary

--- server/src/features/get-week-summary.ts
+++ server/src/features/get-week-summary.ts
@@ -1,5 +1,82 @@
-export async function getWeekSummary() {
+import { db } from "../db";
+import dayjs from "dayjs";
+import weekOfYear from "dayjs/plugin/weekOfYear";
+import { completedGoals, goals } from "../db/schema";
+import { lte, gte, and, count, eq, sql } from "drizzle-orm"
+
+
+
+export async function getWeekPendingGoals() {
+    const firstDayOfWeek = dayjs().startOf('week').toDate();
+    const lastDayOfWeek = dayjs().endOf('week').toDate();
+
+    // Subconsulta para pegar as metas criadas até o final da semana
+    const goalsCreatedUpToWeek = db.$with('goals_created_up_to_week').as(
+        db.select({
+            id: goals.id,
+            title: goals.title,
+            desiredWeeklyFrequency: goals.desiredWeeklyFrequency,
+            createdAt: goals.createdAt,
+        })
+        .from(goals)
+        .where(lte(goals.createdAt, lastDayOfWeek))
+    );
+
+    // Subconsulta para contar as metas completadas durante a semana
+    const goalsCompletedInWeek = db.$with('goals_completed_in_week').as(
+        db.select({
+            id: completedGoals.id,
+            title: goals.title,
+            completedAt: completedGoals.createdAt,
+            completedAtDate: sql/*sql*/`
+                DATE(${completedGoals.createdAt})
+            `.as('completedAtDate')
+           
+        })
+        .from(completedGoals)
+        .innerJoin(goals, eq(goals.id, completedGoals.goalId))
+        .where(
+            and(
+                gte(completedGoals.createdAt, firstDayOfWeek),
+                lte(completedGoals.createdAt, lastDayOfWeek)
+            )
+        )
+    );
+
+    const goalsCompletedByWeekDay = db.$with('goals_completed_by_week_day').as(
+        db.select({
+            completedAtDate: goalsCompletedInWeek.completedAtDate,
+            completions: sql`
+                JSON_AGG(
+                    JSON_BUILD_OBJECT(
+                        'id', ${goalsCompletedInWeek.id},
+                        'title', ${goalsCompletedInWeek.title},
+                        'completedAt', ${goalsCompletedInWeek.completedAt}
+                    )
+                )
+            `.as('completions')
+        }).from(goalsCompletedInWeek)
+        .groupBy(goalsCompletedInWeek.completedAtDate)
+    )
+
+    const result = await db
+        .with(goalsCreatedUpToWeek, goalsCompletedInWeek, goalsCompletedByWeekDay)
+        .select({
+            completed: sql
+            `(SELECT COUNT(*) FROM ${goalsCompletedInWeek})`.mapWith(Number),
+
+            total: sql
+            `(SELECT SUM(${goalsCreatedUpToWeek.desiredWeeklyFrequency}) FROM ${goalsCreatedUpToWeek})`.mapWith(Number),
+
+            goalsPerDay: sql
+            `JSON_OBJECT_AGG(
+                ${goalsCompletedByWeekDay.completedAtDate},
+                ${goalsCompletedByWeekDay.completions}
+            )`
+        })
+        .from(goalsCompletedByWeekDay)
+
     return {
-        summary: 'teste'
+        summary: result
     }
 }
--- server/src/http/routes/get-week-summary.routes.ts
+++ server/src/http/routes/get-week-summary.routes.ts
@@ -1,10 +1,10 @@
 import type { FastifyPluginAsyncZod } from 'fastify-type-provider-zod';
 
-import { getWeekSummary } from '../../features/get-week-summary';
+import { getWeekPendingGoals } from '../../features/get-week-summary';
 
 export const getWeekSummaryRoute: FastifyPluginAsyncZod = async (app) => {
     app.get('/summary', async () => {
-        const { summary } = await getWeekSummary()
+        const { summary } = await getWeekPendingGoals()
    
         return { summary }
    })
