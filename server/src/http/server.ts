e27d5898514ecc640071745a65542a035eec2e4e
Yuri Sousa <ysousa.dev@gmail.com>
2024-09-18 17:23:51 -0300

    feat: :sparkles: configura o cors

--- server/src/http/server.ts
+++ server/src/http/server.ts
@@ -1,15 +1,20 @@
 import fastify from "fastify";
 import { serializerCompiler, validatorCompiler, type ZodTypeProvider } from "fastify-type-provider-zod";
+import { getWeekSummaryRoute } from "./routes/get-week-summary.routes";
+import fastifyCors from "@fastify/cors";
 
 import { createGoalRoute } from "./routes/create-a-goal.routes";
 import { createGoalCompletionRoute } from "./routes/complete-a-goal.routes";
 import { getPendingGoalsRoute } from "./routes/get-pending-goals.routes";
-import { getWeekSummaryRoute } from "./routes/get-week-summary.routes";
 
 const app = fastify().withTypeProvider<ZodTypeProvider>();
 
 app.setValidatorCompiler(validatorCompiler);
 app.setSerializerCompiler(serializerCompiler);
+
+app.register(fastifyCors, {
+	origin: '*'
+})
 
 app.register(createGoalRoute)
 app.register(createGoalCompletionRoute)
