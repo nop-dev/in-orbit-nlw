14e5f3a6e86629b185fa942bb4e4fcf58f34e61b
Yuri Sousa <ysousa.dev@gmail.com>
2024-09-18 22:48:35 -0300

    feat: :sparkles: adiciona react-icons e página empty

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,6 +1,21 @@
-export function App() {
+import { GoPlus } from "react-icons/go";
+import ilustration from "./assets/illustration.svg";
+import logoname from "./assets/logoname.svg";
 
-  return (
-    <h1>Hello World!</h1>
-  )
+export function App() {
+	return (
+		<div className="h-screen flex flex-col items-center justify-center gap-8 font-inter">
+			<img src={logoname} alt="in.orbit" />
+			<img src={ilustration} alt="Let's start!" />
+
+			<p className="text-zinc-300 leading-relaxed max-w-80 text-center">
+				Você ainda não cadastrou nenhuma meta, que tal cadastrar um agora mesmo?
+			</p>
+
+			<button type="button" className="px-4 py-2.5 rounded-lg bg-violet-500 text-violet-50 flex items-center gap-1.5 text-sm font-medium tracking-tight hover:bg-violet-600">
+				<GoPlus className="size-4" />
+				Cadastrar meta
+			</button>
+		</div>
+	);
 }
--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,9 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { App } from './App'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import './index.css';
+import { App } from "./App";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+// biome-ignore lint/style/noNonNullAssertion: <explanation>
+createRoot(document.getElementById("root")!).render(
+	<StrictMode>
+		<App />
+	</StrictMode>,
+);
