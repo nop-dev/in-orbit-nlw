3f3a8ce4ed557b0e14d36ba2a7a2c60173491232
Yuri Sousa <ysousa.dev@gmail.com>
2024-09-23 17:55:49 -0300

    feat: :sparkles: também componentiza tela sem metas (empty)

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,30 +1,11 @@
-import { GoPlus } from "react-icons/go";
-import ilustration from "./assets/illustration.svg";
-import logoname from "./assets/logoname.svg";
-
-import { Button } from "./components/ui/button";
-import { Dialog, DialogTrigger } from "./components/ui/dialog";
 import { CreateGoal } from "./components/create-goal";
-
+import { EmptyGoals } from "./components/empty-goals";
+import { Dialog } from "./components/ui/dialog";
 
 export function App() {
 	return (
 		<Dialog>
-				<div className="h-screen flex flex-col items-center justify-center gap-8 font-inter">
-					<img src={logoname} alt="in.orbit" />
-					<img src={ilustration} alt="Let's start!" />
-
-					<p className="text-zinc-300 leading-relaxed max-w-80 text-center">
-						Você ainda não cadastrou nenhuma meta, que tal cadastrar um agora mesmo?
-					</p>
-
-					<DialogTrigger asChild>
-						<Button>
-								<GoPlus className="size-4" />
-								Cadastrar meta
-						</Button>
-					</DialogTrigger>
-				</div>
+				<EmptyGoals />
 
 				<CreateGoal />
 		</Dialog>
---
+++ web/src/components/empty-goals.tsx
@@ -0,0 +1,27 @@
+import { GoPlus } from "react-icons/go";
+import ilustration from "../assets/illustration.svg";
+import logoname from "../assets/logoname.svg";
+
+import { Button } from "../components/ui/button";
+import { DialogTrigger } from "./ui/dialog";
+
+export function EmptyGoals() {
+  return(
+    <div className="h-screen flex flex-col items-center justify-center gap-8 font-inter">
+					<img src={logoname} alt="in.orbit" />
+					<img src={ilustration} alt="Let's start!" />
+
+					<p className="text-zinc-300 leading-relaxed max-w-80 text-center">
+						Você ainda não cadastrou nenhuma meta, que tal cadastrar um agora mesmo?
+					</p>
+
+					<DialogTrigger asChild>
+						<Button>
+								<GoPlus className="size-4" />
+								Cadastrar meta
+						</Button>
+					</DialogTrigger>
+				</div>
+  )
+  
+}
