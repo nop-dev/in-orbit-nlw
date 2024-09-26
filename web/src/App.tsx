96d965ed4dcbf6b71f9a4aabdb881337bb6ca614
Yuri Sousa <ysousa.dev@gmail.com>
2024-09-25 21:15:47 -0300

    feat: :sparkles: adiciona tanstack para lidar com fetches, organiza requisições

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,39 +1,18 @@
-import { useEffect, useState } from "react";
+import { useQuery } from "@tanstack/react-query";
 import { EmptyGoals } from "./components/empty-goals";
 import { Summary } from "./components/summary";
 import { Dialog } from "./components/ui/dialog";
-
-type SummaryResponse = {
-	completed: number;
-	total: number;
-	goalsPerDay: Record<
-		string,
-		{
-			id: string;
-			title: string;
-			completedAt: string;
-		}[]
-	>;
-};
+import { getSummary } from "./http/get-summary";
 
 export function App() {
-	const [summary, setSummary] = useState<SummaryResponse | null>(null);
-
-	useEffect(() => {
-		fetch("http://localhost:3331/summary")
-			.then((response) => {
-				return response.json();
-			})
-			.then((data) => {
-				setSummary(data.summary);
-			});
-	}, []);
-
-	console.log(summary);
+	const { data } = useQuery({
+		queryKey: ["summary"],
+		queryFn: getSummary,
+	});
 
 	return (
 		<Dialog>
-			{summary?.total && summary.total > 0 ? <Summary /> : <EmptyGoals />}
+			{data?.total && data.total > 0 ? <Summary /> : <EmptyGoals />}
 		</Dialog>
 	);
 }
--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,11 +1,13 @@
-import { StrictMode } from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { createRoot } from "react-dom/client";
-import './index.css';
 import { App } from "./App";
+import "./index.css";
+
+const queryClient = new QueryClient();
 
 // biome-ignore lint/style/noNonNullAssertion: <explanation>
 createRoot(document.getElementById("root")!).render(
-	<StrictMode>
+	<QueryClientProvider client={queryClient}>
 		<App />
-	</StrictMode>,
+	</QueryClientProvider>,
 );
