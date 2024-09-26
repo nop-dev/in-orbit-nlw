34336e8a7c5e6f12b273061e920576eae28949c9
Yuri Sousa <ysousa.dev@gmail.com>
2024-09-25 22:08:11 -0300

    feat: :sparkles: adiciona requisição e funcionalidade de marcar as tarefas como concluidas

--- web/src/components/peding-goals.tsx
+++ web/src/components/peding-goals.tsx
@@ -2,6 +2,7 @@ import { useQuery } from "@tanstack/react-query";
 import { GoPlus } from "react-icons/go";
 import { getPendingGoals } from "../http/get-pending-goals";
 import { OutlineButton } from "./ui/outline-button";
+import { createGoalCompletion } from "../http/create-goal-completion";
 
 export function PendingGoals() {
 	const { data } = useQuery({
@@ -13,6 +14,10 @@ export function PendingGoals() {
 		return null;
 	}
 
+  async function handleCompleteGoal(goalId: string) {
+    await createGoalCompletion(goalId)
+  }
+
 	return (
 		<div className="flex gap-3 flex-wrap">
 			{data.map((goal) => {
@@ -20,6 +25,7 @@ export function PendingGoals() {
 					<OutlineButton
 						key={goal.id}
 						disabled={goal.completedCount >= goal.desiredWeeklyFrequency}
+            onClick={() => handleCompleteGoal(goal.id)}
 					>
 						<GoPlus className="size-4 text-zinc-600" />
 						{goal.title}
