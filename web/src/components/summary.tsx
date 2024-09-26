64b8fd9a9a38fa28051e6f494b3cda5ebadf296f
Yuri Sousa <ysousa.dev@gmail.com>
2024-09-25 21:44:26 -0300

    feat: :sparkles: completa a listagem das metas concluídas

--- web/src/components/summary.tsx
+++ web/src/components/summary.tsx
@@ -20,13 +20,13 @@ export function Summary() {
 	});
 
 	if (!data) {
-		return null;  
+		return null;
 	}
 
 	const firstDayOfWeek = dayjs().startOf("week").format("D MMM");
 	const lastDayOfWeek = dayjs().endOf("week").format("D MMM");
 
-	const completedPercentage = Math.round(data.completed * 100 / data.total)
+	const completedPercentage = Math.round((data.completed * 100) / data.total);
 
 	return (
 		<div className="py-10 max-w-[480px] px-5 mx-auto flex flex-col gap-6">
@@ -88,58 +88,38 @@ export function Summary() {
 
 			<div className="flex flex-col gap-6">
 				<h2 className="text-xl font-medium">Sua semana</h2>
-
-				<div className="flex flex-col gap-4">
-					<h3 className="font-medium">
-						Terça <span className="text-zinc-400 text-xs">(10 de Agosto)</span>
-					</h3>
-
-					<ul className="flex flex-col gap-3">
-						<li className="flex items-center gap-2">
-							<GoCheckCircle className="size-4 text-pink-500" />
-							<span className="text-sm text-zinc-400">
-								Você completou{" "}
-								<span className="text-zinc-100">"Acordar Cedo"</span> as{" "}
-								<span className="text-zinc-100">07:15</span>
-							</span>
-						</li>
-					</ul>
-				</div>
-
-				<div className="flex flex-col gap-4">
-					<h3 className="font-medium">
-						Quinta <span className="text-zinc-400 text-xs">(10 de Agosto)</span>
-					</h3>
-
-					<ul className="flex flex-col gap-3">
-						<li className="flex items-center gap-2">
-							<GoCheckCircle className="size-4 text-pink-500" />
-							<span className="text-sm text-zinc-400">
-								Você completou{" "}
-								<span className="text-zinc-100">"Acordar Cedo"</span> as{" "}
-								<span className="text-zinc-100">07:15h</span>
-							</span>
-						</li>
-					</ul>
-				</div>
-
-				<div className="flex flex-col gap-4">
-					<h3 className="font-medium">
-						Sexta <span className="text-zinc-400 text-xs">(10 de Agosto)</span>
-					</h3>
-
-					<ul className="flex flex-col gap-3">
-						<li className="flex items-center gap-2">
-							<GoCheckCircle className="size-4 text-pink-500" />
-							<span className="text-sm text-zinc-400">
-								Você completou{" "}
-								<span className="text-zinc-100">"Acordar Cedo"</span> as{" "}
-								<span className="text-zinc-100">07:15h</span>
-							</span>
-						</li>
-					</ul>
-				</div>
 			</div>
+
+			{Object.entries(data.goalsPerDay).map(([date, goals]) => {
+        const weekDay = dayjs(date).format('dddd')
+        const formatedDate = dayjs(date).format('D [ de ] MMMM')
+
+				return (
+					<div key={date} className="flex flex-col gap-4">
+						<h3 className="font-medium">
+							 <span className="capitalize">{weekDay}{" "}</span>
+							<span className="text-zinc-400 text-xs">({formatedDate})</span>
+						</h3>
+
+						<ul className="flex flex-col gap-3">
+							{ goals.map(goal => {
+                const time = dayjs(goal.completedAt).format('HH:mm[h]')
+
+                return (
+                  <li key={goal.id} className="flex items-center gap-2">
+								<GoCheckCircle className="size-4 text-pink-500" />
+								<span className="text-sm text-zinc-400">
+									Você completou{" "}
+									<span className="text-zinc-100">"{goal.title}"</span> as{" "}
+									<span className="text-zinc-100">{time}</span>
+								</span>
+							</li>
+                )
+              })}
+						</ul>
+					</div>
+				);
+			})}
 		</div>
 	);
 }
