b5ed436cc139f60d21f54d47ec530c10d62a4859
Yuri Sousa <ysousa.dev@gmail.com>
2024-09-25 21:30:14 -0300

    feat: :sparkles: ultiliza da requisição criada pra preencher primeiros dados (cabeçalho) da página de metas
    também adiciona dayjs

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -8,6 +8,7 @@ export function App() {
 	const { data } = useQuery({
 		queryKey: ["summary"],
 		queryFn: getSummary,
+		staleTime: 1000 * 60 // 60 secs
 	});
 
 	return (
--- web/src/components/summary.tsx
+++ web/src/components/summary.tsx
@@ -1,101 +1,145 @@
-import { GoPlus, GoCheckCircle } from "react-icons/go";
+import { useQuery } from "@tanstack/react-query";
+import dayjs from "dayjs";
+import ptBr from "dayjs/locale/pt-BR";
+import { GoCheckCircle, GoPlus } from "react-icons/go";
+import { getSummary } from "../http/get-summary";
+import { InOrbitIcon } from "./in-orbit-icon";
 import { Button } from "./ui/button";
 import { DialogTrigger } from "./ui/dialog";
-import { InOrbitIcon } from "./in-orbit-icon";
+import { OutlineButton } from "./ui/outline-button";
 import { Progress, ProgressIndicator } from "./ui/progress-bar";
 import { Separator } from "./ui/separator";
-import { OutlineButton } from "./ui/outline-button";
+
+dayjs.locale(ptBr);
 
 export function Summary() {
-  return(
-    <div className="py-10 max-w-[480px] px-5 mx-auto flex flex-col gap-6">
-      <div className="flex items-center justify-between">
-        <div className="flex items-center gap-3">
-          <span className="text-lg font-semibold">5 a 10 de Agosto</span>
-          <InOrbitIcon />
-        </div>
-        
+	const { data } = useQuery({
+		queryKey: ["summary"],
+		queryFn: getSummary,
+		staleTime: 1000 * 60, // 60 secs
+	});
 
-      <DialogTrigger asChild>
-						<Button size="sm">
-								<GoPlus className="size-4" />
-								Cadastrar meta
-						</Button>
-					</DialogTrigger>
-      </div>
+	if (!data) {
+		return null;  
+	}
 
-      <div className="flex flex-col gap-3">
-        <Progress max={15} value={8}>
-          <ProgressIndicator style={ { width: '50%' } }/>
-        </Progress>
+	const firstDayOfWeek = dayjs().startOf("week").format("D MMM");
+	const lastDayOfWeek = dayjs().endOf("week").format("D MMM");
 
-        <div className="flex items-center justify-between text-xs text-zinc-400">
-          <span>Você completou <span className="text-zinc-100">8</span> de <span className="text-zinc-100">15</span> metas nessa semana.</span>
-          <span>50%</span>
-        </div>
-      </div>
+	const completedPercentage = Math.round(data.completed * 100 / data.total)
 
-      <Separator />
+	return (
+		<div className="py-10 max-w-[480px] px-5 mx-auto flex flex-col gap-6">
+			<div className="flex items-center justify-between">
+				<div className="flex items-center gap-3">
+					<span className="text-lg font-semibold capitalize">
+						{firstDayOfWeek} - {lastDayOfWeek}
+					</span>
+					<InOrbitIcon />
+				</div>
 
-      <div className="flex gap-3 flex-wrap">
-        <OutlineButton>
-          <GoPlus className="size-4 text-zinc-600" />
-          Estudar
-        </OutlineButton>
+				<DialogTrigger asChild>
+					<Button size="sm">
+						<GoPlus className="size-4" />
+						Cadastrar meta
+					</Button>
+				</DialogTrigger>
+			</div>
 
-        <OutlineButton>
-          <GoPlus className="size-4 text-zinc-600" />
-          Praticar exercício
-        </OutlineButton>
+			<div className="flex flex-col gap-3">
+				<Progress max={15} value={8}>
+					<ProgressIndicator style={{ width: `${completedPercentage}%` }} />
+				</Progress>
 
-        <OutlineButton>
-          <GoPlus className="size-4 text-zinc-600" />
-          Arumar o quarto
-        </OutlineButton>
+				<div className="flex items-center justify-between text-xs text-zinc-400">
+					<span>
+						Você completou{" "}
+						<span className="text-zinc-100">{data?.completed}</span> de{" "}
+						<span className="text-zinc-100">{data?.total}</span> metas nessa
+						semana.
+					</span>
+					<span>{completedPercentage}%</span>
+				</div>
+			</div>
 
-        <OutlineButton>
-          <GoPlus className="size-4 text-zinc-600" />
-          Lavar a louça
-        </OutlineButton>
-      </div>
+			<Separator />
 
-      <div className="flex flex-col gap-6">
-        <h2 className="text-xl font-medium">Sua semana</h2>
+			<div className="flex gap-3 flex-wrap">
+				<OutlineButton>
+					<GoPlus className="size-4 text-zinc-600" />
+					Estudar
+				</OutlineButton>
 
-        <div className="flex flex-col gap-4">
-          <h3 className="font-medium">Terça <span className="text-zinc-400 text-xs">(10 de Agosto)</span></h3>
+				<OutlineButton>
+					<GoPlus className="size-4 text-zinc-600" />
+					Praticar exercício
+				</OutlineButton>
 
-          <ul className="flex flex-col gap-3">
-            <li className="flex items-center gap-2">
-              <GoCheckCircle className="size-4 text-pink-500" />
-              <span className="text-sm text-zinc-400">Você completou <span className="text-zinc-100">"Acordar Cedo"</span> as <span className="text-zinc-100">07:15</span></span>
-            </li>
-          </ul>
-        </div>
+				<OutlineButton>
+					<GoPlus className="size-4 text-zinc-600" />
+					Arumar o quarto
+				</OutlineButton>
 
-        <div className="flex flex-col gap-4">
-          <h3 className="font-medium">Quinta <span className="text-zinc-400 text-xs">(10 de Agosto)</span></h3>
+				<OutlineButton>
+					<GoPlus className="size-4 text-zinc-600" />
+					Lavar a louça
+				</OutlineButton>
+			</div>
 
-          <ul className="flex flex-col gap-3">
-            <li className="flex items-center gap-2">
-              <GoCheckCircle className="size-4 text-pink-500" />
-              <span className="text-sm text-zinc-400">Você completou <span className="text-zinc-100">"Acordar Cedo"</span> as <span className="text-zinc-100">07:15h</span></span>
-            </li>
-          </ul>
-        </div>
+			<div className="flex flex-col gap-6">
+				<h2 className="text-xl font-medium">Sua semana</h2>
 
-        <div className="flex flex-col gap-4">
-          <h3 className="font-medium">Sexta <span className="text-zinc-400 text-xs">(10 de Agosto)</span></h3>
+				<div className="flex flex-col gap-4">
+					<h3 className="font-medium">
+						Terça <span className="text-zinc-400 text-xs">(10 de Agosto)</span>
+					</h3>
 
-          <ul className="flex flex-col gap-3">
-            <li className="flex items-center gap-2">
-              <GoCheckCircle className="size-4 text-pink-500" />
-              <span className="text-sm text-zinc-400">Você completou <span className="text-zinc-100">"Acordar Cedo"</span> as <span className="text-zinc-100">07:15h</span></span>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </div>
-  )
+					<ul className="flex flex-col gap-3">
+						<li className="flex items-center gap-2">
+							<GoCheckCircle className="size-4 text-pink-500" />
+							<span className="text-sm text-zinc-400">
+								Você completou{" "}
+								<span className="text-zinc-100">"Acordar Cedo"</span> as{" "}
+								<span className="text-zinc-100">07:15</span>
+							</span>
+						</li>
+					</ul>
+				</div>
 
+				<div className="flex flex-col gap-4">
+					<h3 className="font-medium">
+						Quinta <span className="text-zinc-400 text-xs">(10 de Agosto)</span>
+					</h3>
+
+					<ul className="flex flex-col gap-3">
+						<li className="flex items-center gap-2">
+							<GoCheckCircle className="size-4 text-pink-500" />
+							<span className="text-sm text-zinc-400">
+								Você completou{" "}
+								<span className="text-zinc-100">"Acordar Cedo"</span> as{" "}
+								<span className="text-zinc-100">07:15h</span>
+							</span>
+						</li>
+					</ul>
+				</div>
+
+				<div className="flex flex-col gap-4">
+					<h3 className="font-medium">
+						Sexta <span className="text-zinc-400 text-xs">(10 de Agosto)</span>
+					</h3>
+
+					<ul className="flex flex-col gap-3">
+						<li className="flex items-center gap-2">
+							<GoCheckCircle className="size-4 text-pink-500" />
+							<span className="text-sm text-zinc-400">
+								Você completou{" "}
+								<span className="text-zinc-100">"Acordar Cedo"</span> as{" "}
+								<span className="text-zinc-100">07:15h</span>
+							</span>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	);
 }
